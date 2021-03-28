5236b2bbb19eefbd2ca05281a7d1fef357b6ece7
Demo2886 <Demo2886@yandex.by>
2021-03-28 21:22:15 +0300

    11

---
+++ 11.py
@@ -0,0 +1,15 @@
+from collections import Counter
+import re
+
+
+def main():
+    data = open('log').read()
+
+    pattern = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
+    ips = re.findall(pattern, data)
+
+    result = Counter(ips)
+    print(result)
+
+if __name__ == '__name__':
+    main()
